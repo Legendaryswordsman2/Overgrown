e9c2cf888e1ea35a13a3bdaf2006d48703116e1f
LegendarySwordsman2 <LegendarySwordsmen2@gmail.com>
2022-03-17 22:56:34 -0700

    no message

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -4,5 +4,8 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-  
+	[Header("Starting Stats")]
+	[SerializeField] int maxHealth = 100;
+	[SerializeField] int defense = 0;
+	[SerializeField] int damage = 10;
 }
