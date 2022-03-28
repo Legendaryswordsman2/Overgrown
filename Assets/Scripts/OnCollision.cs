e0d98020933b4c87dc8a0ff7a8a7b44a2473fbb6
LegendarySwordsman2 <LegendarySwordsmen2@gmail.com>
2022-03-27 20:12:24 -0700

    no message

--- Assets/Scripts/OnCollision.cs
+++ Assets/Scripts/OnCollision.cs
@@ -5,4 +5,59 @@ using UnityEngine.Events;
 
 public class OnCollision : MonoBehaviour
 {
+	[Tooltip("The game object that collides with this game object must have the specified tag")]
+	[SerializeField] string tag;
+
+	[SerializeField] UnityEvent onCollisionEnter;
+	[SerializeField] UnityEvent onCollisionExit;
+
+	[SerializeField] UnityEvent onTriggerEnter;
+	[SerializeField] UnityEvent onTriggerExit;
+
+	[SerializeField] KeyCode buttonPressKey = KeyCode.None;
+	[SerializeField] UnityEvent onTriggerEnterAndButtonPress;
+
+	bool isInRanged = false;
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (tag == "" || !collision.gameObject.CompareTag(tag)) return;
+
+		Debug.Log("Collision Entered");
+		onCollisionEnter.Invoke();
+	}
+
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+		if (tag == "" || !collision.gameObject.CompareTag(tag)) return;
+
+		Debug.Log("Collision Exited");
+		onCollisionExit.Invoke();
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (tag == "" || !collision.gameObject.CompareTag(tag)) return;
+
+		Debug.Log("Trigger Entered");
+		isInRanged = true;
+		onTriggerEnter.Invoke();
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (tag == "" || !collision.gameObject.CompareTag(tag)) return;
+
+		Debug.Log("Trigger Exited");
+		isInRanged = false;
+		onTriggerExit.Invoke();
+	}
+
+	private void Update()
+	{
+		if (!isInRanged || buttonPressKey == KeyCode.None) return;
+
+		if (Input.GetKeyDown(buttonPressKey))
+			onTriggerEnterAndButtonPress.Invoke();
+	}
 }
