beb1e685db0cc8d27fabe6904a85379be6e8695b
LegendarySwordsman2 <LegendarySwordsmen2@gmail.com>
2022-03-29 13:27:36 -0700

    no message

--- Assets/Scripts/Inventory/PlantInfoBox.cs
+++ Assets/Scripts/Inventory/PlantInfoBox.cs
@@ -8,11 +8,21 @@ public class PlantInfoBox : MonoBehaviour
 	[SerializeField] TMP_Text plantName;
 	[SerializeField] ProgressBar plantHealthBar;
 
+	[SerializeField] TMP_Text healthTextStat;
+	[SerializeField] TMP_Text damageTextStat;
+	[SerializeField] TMP_Text defenseTextStat;
+	[SerializeField] TMP_Text critChanceTextStat;
+
 	public void SetInfoBox(SOPlant plantSO)
 	{
 		plantName.text = plantSO.unitName.ToUpper();
 		plantHealthBar.max = plantSO.defaultHealth;
 		plantHealthBar.current = plantSO.currentHealth;
+
+		healthTextStat.text = "Health: " + plantSO.currentHealth + " / " + plantSO.defaultHealth;
+		damageTextStat.text = "Damage: " + plantSO.meleeDamage;
+		defenseTextStat.text = "Defense: " + plantSO.defense;
+		critChanceTextStat.text = "Crit: " + plantSO.critChance;
 
 		gameObject.SetActive(true);
 	}
